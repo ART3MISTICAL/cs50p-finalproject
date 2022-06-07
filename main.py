3cf9415ee6ce82f73abeaaf93c8c46a0aac78917
ART3MISTICAL <dev.marvel.avengers@gmail.com>
2022-06-07 22:52:00 +0530

    update

--- main.py
+++ main.py
@@ -8,58 +8,58 @@ f = open('user_pass.py', 'ab+')
 f1 = open('user_pass.py', 'a+')
 
 def login():
-    user = input('Enter your username: ')
-    password = input('Enter password: ')
-    try:
-        if password == user_pass[user]:
-            print('Logged in')
-            loggedin = True
+	user = input('Enter your username: ')
+	password = input('Enter password: ')
+	try:
+		if password == user_pass[user]:
+			print('Logged in')
+			loggedin = True
 
-        else:
-            print('The password is incorrect' )
-            loggedin = False
-    
-    except:
-        print('The user doesn\'t exist')
-        loggedin = False
-    
-    return user, loggedin
-    
+		else:
+			print('The password is incorrect' )
+			loggedin = False
+
+	except:
+		print('The user doesn\'t exist')
+		loggedin = False
+
+	return user, loggedin
+
 def start(user):
-    print(f'Welcome {user}')
+	print(f'Welcome {user}')
 
 def set_user():
-    # user_pass[input('New user: ')] = input('Password: ')
-    new_user = input('New username: ')
-    if new_user in user_pass:
-        sys.exit('User already exists')
+	# user_pass[input('New user: ')] = input('Password: ')
+	new_user = input('New username: ')
+	if new_user in user_pass:
+		sys.exit('User already exists')
 
-    new_user_pass = input('Password: ')
-    f.seek(-1, os.SEEK_END)
-    f.truncate()
-    f1.write(f'    \'{new_user}\' : \'{new_user_pass}\', \n{curly}')
-    f.flush()
-    f1.flush()
-    f.close()
-    f1.close()
+	new_user_pass = input('Password: ')
+	f.seek(-1, os.SEEK_END)
+	f.truncate()
+	f1.write(f'    \'{new_user}\' : \'{new_user_pass}\', \n{curly}')
+	f.flush()
+	f1.flush()
+	f.close()
+	f1.close()
 
 def main():
-    logInOrCreate = input('Press and enter "l" to login or "c" to create an account: ')
+	logInOrCreate = input('Press and enter "l" to login or "c" to create an account: ')
 
 
-    if logInOrCreate.lower().strip() == 'l':
-        user, loggedin = login()
+	if logInOrCreate.lower().strip() == 'l':
+		user, loggedin = login()
 
-    elif logInOrCreate.lower().strip() == 'c':
-        set_user()
-        print('User created')
-        sys.exit(0)
+	elif logInOrCreate.lower().strip() == 'c':
+		set_user()
+		print('User created')
+		sys.exit(0)
 
-    else:
-        sys.exit('An error occurred, try again')
+	else:
+		sys.exit('An error occurred, try again')
 
-    if loggedin == True:
-        start(user) 
+	if loggedin == True:
+		start(user)
 
 if __name__ == '__main__':
-    main()
+	main()
